484000d82acb96cb9b3203fbd0164b69b34c9f62
dsy-sw <dsy94.sw@gmail.com>
2022-05-25 19:02:34 +0900

    update module state

--- config/config.py
+++ config/config.py
@@ -1,10 +1,21 @@
-class ADRRESS:
-    SERVER_ADRESS = '192.168.1.1'
-    SYSTEM_ADRRESS = {'(192.168.1.1,8000)':'WEB', '(192.168.1.255,30001)':'SQL'}
-    DEVICE_ADRRESS = {'(192.168.1.10,10000)':'MOTION', '(192.168.1.20,10001)':'BUTTON', '(192.168.1.100,20000)':'LIGHT'}
+class ADDRESS:
+    SERVER_ADDRESS = '192.168.1.1'
+    SYSTEM_ADDRESS = {'(192.168.1.1,8000)':'WEB', '(192.168.1.255,30001)':'SQL'}
+    DEVICE_ADDRESS = {'(192.168.1.10,10000)':'MOTION', '(192.168.1.20,10001)':'BUTTON', '(192.168.1.100,20000)':'LIGHT'}
 
 class MqttConfig:
+    BROKER_HOST = '127.0.0.1'
+
+    LOCATION = {'192.168.1':'house/admin'}
+
     SERVER_TOPIC = '#'
     SENSOR_TOPIC = 'sensor/#'
     LIGHT_TOPIC = 'light/#'
     BUTTON_TOPIC = 'button/#'
+
+class ModuleState:
+    # 0.input
+    INPUT_STATE = True
+
+    # 1.network
+    NETWORK_STATE = True
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ctypes import windll
 from src.mcu.mcu import *
 from src.sql.sql import *
 from src.web.web import *
+from config.config import ModuleState
 
 timeBeginPeriod = windll.winmm.timeBeginPeriod
 timeBeginPeriod(1,)
@@ -14,8 +15,33 @@ class IoT():
     def __init__(self):
         self.name = None
 
+    def input_module(self):
+        pass
+
+    def network_module(self):
+        pass
 
     def run():
+        inputState = ModuleState.INPUT_STATE
+        networkState = ModuleState.NETWORK_STATE
+
+        while 1:
+            # 0.input
+            if inputState:
+                inputState = False
+                input_module = Thread(target = input_module, deamon = True)
+                input_module.start()
+
+            # 1.network
+            if networkState:
+                networkState = False
+                network_module = Thread(target = network_module, deamon = True)
+                network_module.start()
+
+            # 2.video
+
+            # 3.web
+        
         pass
 
 if __name__ == '__main__':
@@ -28,7 +54,6 @@ if __name__ == '__main__':
     #  To be scheduled
     # appQueue = Queue()
     
-    mcuProcess = Process(target = run_mcu_process, name = 'MCU_Process', daemon=True, args = (mcuQueue, syncTime,))
     webProcess = Process(target = run_web_process, name = 'WEB_Process', daemon=True, args = (webQueue, syncTime,))
     sqlProcess = Process(target = run_sql_process, name = 'SQL_Process', daemon=True, args = (sqlQueue, syncTime,))
     
