25afbfca1e987ca4ac0ed4498fdb90bc6ba3a664
dsy-sw <dsy94.sw@gmail.com>
2022-05-25 19:01:57 +0900

    update callback in mqtt class

--- src/pi/connect.py
+++ src/pi/connect.py
@@ -69,6 +69,42 @@ class UdpServer():
         self.port = port
         
 class Mqtt():
-    def __init(self):
-        mqttC = mqtt.Client()
-        
+    """
+    Topic -> Location/Sensor_Type/Subject
+    ex) house/room/switch/light
+    """
+    def __init(self, ip, queue):
+        self.mqttC = mqtt.Client()
+        self.queue_msg = queue
+        self.mqttC.on_connect = self.on_connect
+        self.mqttC.on_message = self.on_message
+        self.ip = ip
+        #
+
+    def on_connect(self):
+        pass
+
+    def on_message(self):
+        pass
+    
+    def subscribe(self):
+        pass
+
+    def publish(self):
+        pass
+
+    def run(self):
+        try:
+            # connect Broker
+            self.mqttC.connect()
+
+            # loop msg
+            self.mqttC.loop_forever()
+        except Exception as err:
+            print(f'{err = }')
+        except KeyboardInterrupt:
+            print('raise interreption')
+
+def process_mqtt(queue_msg, syncTime):
+    networkMQTT = Mqtt(queue_msg, syncTime)
+    networkMQTT.run()
